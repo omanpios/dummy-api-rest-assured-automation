7fd087a4b2e49122ed555678ce6a1cd520929d29
Omar <omanpios88@gmail.com>
2023-01-30 20:39:33 -0500

    Add json schema validation, add new tests, modify API to receive query params, add json validator dependency to POM file

--- src/main/java/api/GetUserList.java
+++ src/main/java/api/GetUserList.java
@@ -6,9 +6,12 @@ import static io.restassured.RestAssured.*;
 public class GetUserList {
     Response response;
 
-    public GetUserList() {
+    public GetUserList(Object page, Object limit) {
         response = given()
                 .header("app-id", "63d1caa3480870720570afb7")
+                .queryParam("page", page = page != null ? page : 0)
+                .queryParam("limit", limit = limit != null ? limit : 0)
+                .log().all()
                 .get("https://dummyapi.io/data/v1/user");
     }
 
--- src/test/java/GetUserListTests.java
+++ src/test/java/GetUserListTests.java
@@ -1,23 +1,45 @@
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+
+import java.io.File;
 
 import api.GetUserList;
+import io.restassured.module.jsv.JsonSchemaValidator;
 
 public class GetUserListTests {
     SoftAssertions softly = new SoftAssertions();
-    GetUserList getUserList = new GetUserList();
 
     @Test
     void verifyThatASuccessfulRequestReturnsA200StatusCode() {
+        GetUserList getUserList = new GetUserList(1, 10);
         softly.assertThat(getUserList.response().statusCode()).isEqualTo(200);
+        softly.assertAll();
     }
 
     @Test
     void verifyThatTheApiImplementsPagination() {
+        GetUserList getUserList = new GetUserList(0, 20);
         softly.assertThat(getUserList.response().getBody().path("limit").toString()).isEqualTo("20");
         softly.assertThat(getUserList.response().getBody().path("total").toString()).isNotEmpty();
         softly.assertThat(getUserList.response().getBody().path("page").toString()).isNotEmpty();
         softly.assertAll();
     }
 
+    @Test
+    void verifyThatAnInvalidAuthenticationReturnsA403StatusCode() {
+        given()
+                .header("app-id", "63d1caa3480870720572222")
+                .get("https://dummyapi.io/data/v1/user")
+                .then().statusCode(403);
+    }
+
+    @Test
+    void verifyThatTheResponseBodyMatchesTheJsonSchema() {
+        given()
+                .header("app-id", "63d1caa3480870720570afb7")
+                .get("https://dummyapi.io/data/v1/user")
+                .then()
+                .body(JsonSchemaValidator.matchesJsonSchema(new File("src/main/java/schema/GetListUserSchema.json")));
+    }
 }
