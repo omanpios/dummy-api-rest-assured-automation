7586078f057112d7986cc9a1ec0a738ba4d20251
Omar <omanpios88@gmail.com>
2023-02-06 07:28:35 -0500

    Remove hardcoded values, fix misspelling error

--- src/test/java/CreateUserTests.java
+++ src/test/java/CreateUserTests.java
@@ -7,66 +7,92 @@ import io.restassured.module.jsv.JsonSchemaValidator;
 import static io.restassured.RestAssured.given;
 
 import java.io.File;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class CreateUserTests {
 
-    Faker fake = new Faker();
+        Faker fake = new Faker();
 
-    @Test
-    void verifyThatASuccessfulRequestBasicDataReturnsA200StatusCode() {
-        given().log().all()
-                .header("app-id", "63d1caa3480870720570afb7")
-                .header("Content-Type", "application/json")
-                .body("{\n\"lastName\":\"" + fake.name().lastName()
-                        + "\",\n\"firstName\":\"" + fake.name().firstName()
-                        + "\",\n\"email\":\"" + fake.internet().safeEmailAddress() + "\"\n}")
-                .post("https://dummyapi.io/data/v1/user/create")
-                .then().log().all()
-                .statusCode(200)
-                .body(JsonSchemaValidator.matchesJsonSchema(new File("src/main/java/schema/GetUserByIdSchema.json")));
-    }
+        @Test
+        void verifyThatASuccessfulRequestBasicDataReturnsA200StatusCode() {
+                given().log().all()
+                                .header("app-id", "63d1caa3480870720570afb7")
+                                .header("Content-Type", "application/json")
+                                .body("{\n\"lastName\":\"" + fake.name().lastName()
+                                                + "\",\n\"firstName\":\"" + fake.name().firstName()
+                                                + "\",\n\"email\":\"" + fake.internet().safeEmailAddress() + "\"\n}")
+                                .post("https://dummyapi.io/data/v1/user/create")
+                                .then().log().all()
+                                .statusCode(200)
+                                .body(JsonSchemaValidator.matchesJsonSchema(
+                                                new File("src/main/java/schema/GetUserByIdSchema.json")));
+        }
 
-    @Test
-    void verifyThatASuccessfulRequestFullDataReturnsA200StatusCode() {
-        given().log().all()
-                .header("app-id", "63d1caa3480870720570afb7")
-                .header("Content-Type", "application/json")
-                .body("{\n\"lastName\":\"" + fake.name().lastName()
-                        + "\",\n\"firstName\":\"" + fake.name().firstName()
-                        + "\",\n\"email\":\"" + fake.internet().safeEmailAddress()
-                        + "\",\n\"picture\":\"" + fake.internet().image()
-                        + "\",\n\"location\": \n {\"street\": \"Street\",\n\"city\": \"Bta\",\n\"state\": \"Street\",\n\"country\": \"Street\",\n\"timezone\": \"9:00\"},\n\"phone\":\""
-                        + fake.phoneNumber().cellPhone() + "\"\n}")
-                .post("https://dummyapi.io/data/v1/user/create")
-                .then().log().all()
-                .statusCode(200)
-                .body(JsonSchemaValidator.matchesJsonSchema(new File("src/main/java/schema/GetUserByIdSchema.json")));
-    }
+        @Test
+        void verifyThatASuccessfulRequestFullDataReturnsA200StatusCode() {
+                String dob = Instant.ofEpochMilli(fake.date().birthday(18, 99).getTime()).toString();
+                Random random = new Random();
+                List<String> title = Arrays.asList("mr", "ms", "mrs", "miss", "dr");
+                List<String> gender = Arrays.asList("male", "female", "other");
 
-    @Test
-    void verifyThatARequestWithAnExistentEmailAddressReturnsA400StatusCode() {
-        given().log().all()
-                .header("app-id", "63d1caa3480870720570afb7")
-                .header("Content-Type", "application/json")
-                .body("{\n\"lastName\":\"" + fake.name().lastName()
-                        + "\",\n\"firstName\":\"" + fake.name().firstName()
-                        + "\",\n\"email\":\"lance.foster@example.com\"\n}")
-                .post("https://dummyapi.io/data/v1/user/create")
-                .then().log().all()
-                .statusCode(400)
-                .body(JsonSchemaValidator.matchesJsonSchema(new File("src/main/java/schema/ErrorMessageSchema.json")));
-    }
+                int randomItemFromTitle = random.nextInt(title.size());
+                String randomTitle = title.get(randomItemFromTitle);
 
-    @Test
-    void verifyThatAnIncalidRequestReturnsA400StatusCode() {
-        given().log().all()
-                .header("app-id", "63d1caa3480870720570afb7")
-                .header("Content-Type", "application/json")
-                .body("{\n\"lastName\":\"D\",\n\"firstName\":\"C\",\n\"email\":\"lance.foster\"\n}")
-                .post("https://dummyapi.io/data/v1/user/create")
-                .then().log().all()
-                .statusCode(400)
-                .body(JsonSchemaValidator.matchesJsonSchema(new File("src/main/java/schema/ErrorMessageSchema.json")));
-    }
+                int randomItemFromGender = random.nextInt(gender.size());
+                String randomGender = gender.get(randomItemFromGender);
+
+                given().log().all()
+                                .header("app-id", "63d1caa3480870720570afb7")
+                                .header("Content-Type", "application/json")
+                                .body("{\n\"lastName\":\"" + fake.name().lastName()
+                                                + "\",\n\"firstName\":\"" + fake.name().firstName()
+                                                + "\",\n\"title\":\"" + randomTitle
+                                                + "\",\n\"gender\":\"" + randomGender
+                                                + "\",\n\"dateOfBirth\":\"" + dob
+                                                + "\",\n\"email\":\"" + fake.internet().safeEmailAddress()
+                                                + "\",\n\"picture\":\"" + fake.internet().image()
+                                                + "\",\n\"location\": \n {\"street\": \"" + fake.address().fullAddress()
+                                                + "\",\n\"city\": \"" + fake.address().cityName()
+                                                + "\",\n\"state\": \"" + fake.address().state()
+                                                + "\",\n\"country\": \"" + fake.address().country()
+                                                + "\",\n\"timezone\": \"" + fake.address().timeZone()
+                                                + "\"},\n\"phone\":\""
+                                                + fake.phoneNumber().cellPhone() + "\"\n}")
+                                .post("https://dummyapi.io/data/v1/user/create")
+                                .then().log().all()
+                                .statusCode(200)
+                                .body(JsonSchemaValidator.matchesJsonSchema(
+                                                new File("src/main/java/schema/GetUserByIdSchema.json")));
+        }
+
+        @Test
+        void verifyThatARequestWithAnExistentEmailAddressReturnsA400StatusCode() {
+                given().log().all()
+                                .header("app-id", "63d1caa3480870720570afb7")
+                                .header("Content-Type", "application/json")
+                                .body("{\n\"lastName\":\"" + fake.name().lastName()
+                                                + "\",\n\"firstName\":\"" + fake.name().firstName()
+                                                + "\",\n\"email\":\"lance.foster@example.com\"\n}")
+                                .post("https://dummyapi.io/data/v1/user/create")
+                                .then().log().all()
+                                .statusCode(400)
+                                .body(JsonSchemaValidator.matchesJsonSchema(
+                                                new File("src/main/java/schema/ErrorMessageSchema.json")));
+        }
+
+        @Test
+        void verifyThatAnInvalidRequestReturnsA400StatusCode() {
+                given().log().all()
+                                .header("app-id", "63d1caa3480870720570afb7")
+                                .header("Content-Type", "application/json")
+                                .body("{\n\"lastName\":\"D\",\n\"firstName\":\"C\",\n\"email\":\"lance.foster\"\n}")
+                                .post("https://dummyapi.io/data/v1/user/create")
+                                .then().log().all()
+                                .statusCode(400)
+                                .body(JsonSchemaValidator.matchesJsonSchema(
+                                                new File("src/main/java/schema/ErrorMessageSchema.json")));
+        }
 }
-// TODO: title, gender, birthdate(IsoDate), fake hardcoded values
