b604b12019d9a934888a2d39d4b88ba3bd40d544
Omar <omanpios88@gmail.com>
2023-02-13 09:27:54 -0500

    Add @JsonInclude annotation to ignore null fields

--- src/main/java/pojo/User.java
+++ src/main/java/pojo/User.java
@@ -1,8 +1,11 @@
 package pojo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.Data;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class User {
     private String id;
     private String title;
