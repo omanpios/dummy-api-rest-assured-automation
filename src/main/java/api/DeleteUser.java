013dafb8298817695adfff7fa7ee007bec8e0f6f
Omar <omanpios88@gmail.com>
2023-02-15 08:30:49 -0500

    Create DeleteUser class to call delete user endpoint

---
+++ src/main/java/api/DeleteUser.java
@@ -0,0 +1,36 @@
+package api;
+
+import static io.restassured.RestAssured.*;
+
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import pojo.DeletedUser;
+
+public class DeleteUser {
+    Response response;
+    private final String baseUri = "https://dummyapi.io/data/v1";
+
+    public DeleteUser(String appId, String userId) {
+        RequestSpecification request = given().log().all()
+                .baseUri(baseUri)
+                .header("app-id", appId);
+
+        response = request.when()
+                .delete("/user/" + userId)
+                .then().log().all()
+                .extract()
+                .response();
+    }
+
+    public DeletedUser deleteResponse() {
+        return response.getBody().as(DeletedUser.class);
+    }
+
+    public Response response() {
+        return response;
+    }
+
+    public Error error() {
+        return response.getBody().as(Error.class);
+    }
+}
